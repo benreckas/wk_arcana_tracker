02da998d1fa02d625c2262341bd2dc60739a69db
Ben Reckas <benreckas@agrian.com>
2020-10-16 09:06:21 -0700

    Minor cleanup

--- wk.rb
+++ wk.rb
@@ -19,29 +19,30 @@ class Tracker
     when 'reset'
       reset
     else
-      hero_search
+      hero_search(@command)
     end
   end
 
   def puts_collected
-    @heroes.each { |hero| puts hero[:name] if hero[:is_collected] }
-    puts_hero_nums
+    @heroes.each { |h| puts h[:name] if h[:is_collected] }
+    puts_hero_totals
   end
 
   def puts_uncollected
-    @heroes.each { |hero| puts hero[:name] unless hero[:is_collected] }
-    puts_hero_nums
+    @heroes.each { |h| puts h[:name] unless h[:is_collected] }
+    puts_hero_totals
   end
 
-  def puts_hero_nums
+  def puts_hero_totals
     puts "#{@heroes.select { |h| h[:is_collected] }.length} heroes collected"
     puts "#{@heroes.reject { |h| h[:is_collected] }.length} heroes remaining"
   end
 
-  def hero_search
+  def hero_search(search_string)
     @heroes.each do |hero|
+      # Skip iteration if hero is already collected or hero name doesn't include the search string
       next if hero[:is_collected]
-      next unless hero[:name].downcase.include? @command.downcase
+      next unless hero[:name].downcase.include? search_string.downcase
 
       puts "Collect: #{hero[:name]}?"
       print 'y/n: '
@@ -51,6 +52,13 @@ class Tracker
     end
   end
 
+  def collect_hero(hero)
+    @heroes.map { |h| h[:is_collected] = true if h[:name] == hero[:name] }
+
+    puts "#{hero[:name]} was collected!"
+    overwrite_heroes_file
+  end
+
   def reset
     puts 'Are you sure you want to reset all progress?'
     print 'y/n: '
@@ -58,25 +66,14 @@ class Tracker
 
     return unless should_reset.casecmp('y').zero?
 
-    @heroes.each { |hero| hero[:is_collected] = false }
+    @heroes.map { |h| h[:is_collected] = false }
 
     overwrite_heroes_file
-  end
-
-  def collect_hero(defeated_hero)
-    @heroes.each do |hero|
-      hero[:is_collected] = true if hero[:name] == defeated_hero[:name]
-    end
-
-    overwrite_heroes_file
-
-    puts "#{defeated_hero[:name]} was collected!"
   end
 
   def overwrite_heroes_file
-    File.open('heroes.yml', 'w') do |out|
-      YAML.dump(@heroes, out)
-    end
+    File.open('heroes.yml', 'w') { |out| YAML.dump(@heroes, out) }
+    puts_hero_totals
   end
 end
 
